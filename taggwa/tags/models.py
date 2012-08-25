6f28195213a0bee44febe1d5fad2bc6cb9e3bb76
someone <borko.boyanov@gmail.com>
2012-08-25 09:06:35 +0300

    Update taggwa/tags/models.py

--- taggwa/tags/models.py
+++ taggwa/tags/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from taggwa.common.models import RegisteredType
+from taggwa.resources.models import Resource
+
 # Create your models here.
 
 class Tag(RegisteredType):
     """ Tags. base class """
     the_keyword = models.CharField(default = "", null = True)
-    
+    the_resource = models.ForeignKey(Resource)
     # Put a (tag, tag) relationship later... not needed
     
